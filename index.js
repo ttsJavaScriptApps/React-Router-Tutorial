6a0a7ad78de6e5ef5e4660c2e16b85757109f569
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-03-17 21:03:14 -0400

    @ 03 & added .gitignore

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
 import App from './modules/App'
-ReactDOM.render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+ReactDOM.render((
+  <Router >
+    <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+    <Route path="/repos" component={Repos}/>
+  </Router>
+), document.getElementById('app'))
