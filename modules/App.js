d4104b82a1e294739abc34e4cff26fd03f5b1433
Jude Harding <jharding@genesisgroupusa.com>
2016-03-17 21:01:20 -0400

    through step 10

--- modules/App.js
+++ modules/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
+  render: function() {
+    return (
+      <div>
+        <h1>Ghettohub Issues</h1>
+        <ul role="nav">
+          <li><NavLink to='/' onlyActiveOnIndex>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/repos'>Repos</NavLink></li>
+        </ul>
+        {this.props.children}
+
+      </div>
+    )
   }
 })
---
+++ modules/Repos.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render: function() {
+    return (<div>
+      <h2>Repos</h2>
+        <ul>
+          <li><NavLink to="/repos/rackt/react-router">React Router </NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React </NavLink></li>
+        </ul>
+        {this.props.children}
+    </div>)
+  }
+})
