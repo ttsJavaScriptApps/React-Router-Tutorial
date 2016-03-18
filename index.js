a05b4c410c5eca4a82ebf63c046c0badfbe0c1e5
Matthew Wheeler <augustwest@mw.local>
2016-03-17 21:02:42 -0400

    addds gitignore

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
-ReactDOM.render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    {/* add the routes here */}
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'))
---
+++ modules/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default React.createClass({
+  render: function() {
+    return (
+      <div>About for About</div>
+    );
+  }
+});
+
+// ReactDOM.render(
+//   <About />,
+//   document.getElementById('content')
+// );
