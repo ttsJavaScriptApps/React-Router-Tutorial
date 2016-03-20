371535df4cef9600a4d51e970108debb28035f4f
Matthew Wheeler <augustwest@mw.local>
2016-03-20 16:51:40 -0400

    premerge

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, Link } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
